51db23f015126076612f1efa5f9cae353d42fa8b
Chris Huang <chrishuang128@gmail.com>
2019-04-15 13:12:29 -0700

    Inherit properties from Bee along with modifying/adding properties/methods

--- src/ForagerBee.js
+++ src/ForagerBee.js
@@ -1,3 +1,14 @@
 var ForagerBee = function() {
+  Bee.call(this);
+  this.job = 'find pollen';
+  this.color = 'yellow';
+  this.canFly = true;
+  this.age = 10;
+  this.treasureChest = [];
 };
 
+ForagerBee.prototype = Object.create(Bee.prototype);
+ForagerBee.prototype.forage = function(){
+  this.treasureChest.push('treasure')
+};
+ForagerBee.prototype.constructor = ForagerBee;
