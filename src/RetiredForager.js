37c67095e64c134af18c071597028e1fc4faaf01
Chris Huang <chrishuang128@gmail.com>
2019-04-15 13:18:28 -0700

    Implemented and completed RetiredForager

--- src/RetiredForager.js
+++ src/RetiredForager.js
@@ -1,3 +1,22 @@
 var RetiredForagerBee = function() {
+  ForagerBee.call(this);
+  this.age = 40;
+  this.job = 'gamble';
+  this.canFly = false;
+  this.color = 'grey';
+  this.treasureChest = [];
 };
 
+RetiredForagerBee.prototype = Object.create(Grub.prototype);
+
+RetiredForagerBee.prototype.gamble = function(){
+  if (this.job === 'gamble'){
+    this.treasureChest.push('treasure');
+  }
+}
+
+RetiredForagerBee.prototype.forage = function(){
+  return "I am too old, let me play cards instead";
+}
+
+RetiredForagerBee.prototype.constructor = RetiredForagerBee;
